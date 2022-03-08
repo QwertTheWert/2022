17c90957a013fe2ca282b266b3528bab058e16f0
QwertTheWert <fredrickwill2005@gmail.com>
2022-03-08 17:22:57 +0700

    3/8/22

--- BACK_END_FLASK_AND_PSQL/01_SIMPLE/10_form/run.py
+++ BACK_END_FLASK_AND_PSQL/01_SIMPLE/10_form/run.py
@@ -7,14 +7,16 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/more", methods=["GET","POST"])
+@app.route("/hello")
 def hello():
-	if request.method == "GET":
-		# return "Please submit the form instead!"
-		return redirect(url_for("index", err_msg = "Please submit first."))
 	name = request.form.get("name")
 	return render_template("hello.html", name=name)
 
+@app.route("/search")
+def search():
+	query = request.args.get("query")
+	return redirect("https://www.google.com/search?q="+query)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True, port=8000)
+	
