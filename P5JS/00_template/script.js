cf24543eaa118338141c09229097345663dd5a77
QwertTheWert <fredrickwill2005@gmail.com>
2022-04-15 13:00:20 +0700

    4/14/22

---
+++ P5JS/00_template/script.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(500, 500);
+	background(102);
+
+	fill(0,0,0);
+	circle(250,250,30);
+
+	line(250,265,250,300)
+}
+
+function draw() {
+	
+}
---
+++ P5JS/01_intro/script.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(500, 500);
+	background(102);
+	frameRate(1);
+	circle(30, 30, 20);
+}
+
+function draw() {
+  console.log("HELLO P5JS!")
+	
+}
